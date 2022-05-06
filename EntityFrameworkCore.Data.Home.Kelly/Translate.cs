3d94bbd2b6d872c6b48de837891f7ce8e61f69b9
Garrett Kelly <garrett@home.kelly>
2022-05-05 23:45:08 -0400

    Adds translate class which includes methods to call API endpoints.

---
+++ EntityFrameworkCore.Data.Home.Kelly/Translate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+
+namespace EntityFrameworkCore.Data.Home.Kelly
+{
+    public class Translate
+    {
+        public static async Task<Item> GetItemFromBarcode(string apiRoot, string barcode)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    UriBuilder builder = new UriBuilder(apiRoot + "/item/query/");
+                    builder.Query = "barcode=" + barcode;
+                    string url = builder.ToString();
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Item fetchedItem = JsonSerializer.Deserialize<Item>(responseBody);
+
+                    return fetchedItem;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
